6a35d07164f8d02522909fc6492eae9565a833d4
Michaël Larouche <michael.larouche@gmail.com>
2015-05-31 08:08:01 -0400

    First version of the hack with no fade or stop !

--- create_pcm.bat
+++ create_pcm.bat
@@ -2,4 +2,18 @@
 
 del *.pcm
 
-wav2msu -o tmnt4_msu1-44.pcm som_01_angel_fear.wav
+wav2msu -o tmnt4_msu1-250.pcm tmnt4_open.wav
+wav2msu -o tmnt4_msu1-232.pcm -l 50620 tmnt4_roof.wav
+wav2msu -o tmnt4_msu1-244.pcm tmnt4_downtown.wav
+wav2msu -o tmnt4_msu1-3.pcm tmnt4_sewarage.wav
+wav2msu -o tmnt4_msu1-45.pcm tmnt4_fortress.wav
+wav2msu -o tmnt4_msu1-9.pcm tmnt4_dinosaur.wav
+wav2msu -o tmnt4_msu1-12.pcm tmnt4_pirate.wav
+wav2msu -o tmnt4_msu1-15.pcm tmnt4_train.wav
+wav2msu -o tmnt4_msu1-18.pcm tmnt4_highway.wav
+wav2msu -o tmnt4_msu1-21.pcm tmnt4_starbase.wav
+wav2msu -o tmnt4_msu1-27.pcm tmnt4_lastboss.wav
+wav2msu -o tmnt4_msu1-235.pcm tmnt4_battle.wav
+wav2msu -o tmnt4_msu1-57.pcm tmnt4_elevator.wav
+wav2msu -o tmnt4_msu1-42.pcm tmnt4_staff.wav
+
--- tmnt4_msu1.asm
+++ tmnt4_msu1.asm
@@ -1,12 +1,12 @@
 arch snes.cpu
 
 // MSU memory map I/O
-constant MSU_STATUS($002000)
-constant MSU_ID($002002)
-constant MSU_AUDIO_TRACK_LO($002004)
-constant MSU_AUDIO_TRACK_HI($002005)
-constant MSU_AUDIO_VOLUME($002006)
-constant MSU_AUDIO_CONTROL($002007)
+constant MSU_STATUS($2000)
+constant MSU_ID($2002)
+constant MSU_AUDIO_TRACK_LO($2004)
+constant MSU_AUDIO_TRACK_HI($2005)
+constant MSU_AUDIO_VOLUME($2006)
+constant MSU_AUDIO_CONTROL($2007)
 
 // SPC communication ports
 constant SPC_COMM_0($2140)
@@ -42,9 +42,9 @@ if {defined EMULATOR_VOLUME} {
 // **********
 // * Macros *
 // **********
-// seek converts SNES HiROM address to physical address
+// seek converts SNES LoROM address to physical address
 macro seek(variable offset) {
-  origin (offset & $3FFFFF)
+  origin ((offset & $7F0000) >> 1) | (offset & $7FFF)
   base offset
 }
 
@@ -75,3 +75,94 @@ macro WaitDivResult() {
 // ********
 // * Code *
 // ********
+seek($009D49)
+	jsl MSU_Main
+	
+seek($00F860)
+scope MSU_Main: {
+	php
+	rep #$30
+	pha
+	phx
+	
+	sep #$30
+	CheckMSUPresence(OriginalCode)
+	
+	// Set MSU-1 track
+	txa
+	sta.w MSU_AUDIO_TRACK_LO
+	stz MSU_AUDIO_TRACK_HI
+
+IsMSUReady:
+	lda.w MSU_STATUS
+	and.b #MSU_STATUS_AUDIO_BUSY
+	bne IsMSUReady
+	
+	// Check if the track is missing
+	lda.w MSU_STATUS
+	and.b #MSU_STATUS_TRACK_MISSING
+	bne TrackMissing
+	
+	// Play the song and add repeat if needed
+	jsr TrackNeedLooping
+	sta.w MSU_AUDIO_CONTROL
+	
+	// Set volume
+	lda.b #FULL_VOLUME
+	sta.w MSU_AUDIO_VOLUME
+	
+	// TODO: Stop SPC music
+	
+	rep #$30
+	plx
+	pla
+	plp
+	rtl
+	
+TrackMissing:
+	stz MSU_AUDIO_CONTROL
+	stz MSU_AUDIO_VOLUME
+	
+OriginalCode:
+	rep #$30
+	plx
+	pla
+	plp
+	jsl $009D50
+	rtl
+}
+
+scope TrackNeedLooping: {
+	// Open
+	cpx #$FA
+	beq NoLooping
+	// Bad News
+	cpx #$00
+	beq NoLooping
+	// Attack
+	cpx #$33
+	beq NoLooping
+	// Clear
+	cpx #$EE
+	beq NoLooping
+	// Game Over
+	cpx #$F1
+	beq NoLooping
+	// Good news
+	cpx #$27
+	beq NoLooping
+	// Staff
+	cpx #$2A
+	beq NoLooping
+	// Escape
+	cpx #$3F
+	beq NoLooping
+	// Good
+	cpx #$42
+	beq NoLooping
+	lda.b #$03
+	rts
+NoLooping:
+	lda.b #$01
+	rts
+}
