a2a0e4f324cc284f512cfec97c73c9efd60acdf1
Michaël Larouche <michael.larouche@gmail.com>
2016-01-09 20:25:14 -0500

    Final edit of the music !

--- create_pcm.bat
+++ create_pcm.bat
@@ -4,18 +4,18 @@ del *.pcm
 
 wav2msu -o tmnt4_msu1-250.pcm tmnt4_open.wav
 wav2msu -o tmnt4_msu1-232.pcm -l 50620 tmnt4_roof.wav
-wav2msu -o tmnt4_msu1-244.pcm tmnt4_downtown.wav
-wav2msu -o tmnt4_msu1-3.pcm tmnt4_sewarage.wav
-wav2msu -o tmnt4_msu1-45.pcm tmnt4_fortress.wav
+wav2msu -o tmnt4_msu1-244.pcm -l 250861 tmnt4_downtown.wav
+wav2msu -o tmnt4_msu1-3.pcm -l 483444 tmnt4_sewarage.wav
+wav2msu -o tmnt4_msu1-45.pcm -l 1985204 tmnt4_fortress.wav
 wav2msu -o tmnt4_msu1-9.pcm tmnt4_dinosaur.wav
-wav2msu -o tmnt4_msu1-12.pcm tmnt4_pirate.wav
-wav2msu -o tmnt4_msu1-15.pcm tmnt4_train.wav
-wav2msu -o tmnt4_msu1-18.pcm tmnt4_highway.wav
-wav2msu -o tmnt4_msu1-21.pcm tmnt4_starbase.wav
-wav2msu -o tmnt4_msu1-27.pcm tmnt4_lastboss.wav
-wav2msu -o tmnt4_msu1-235.pcm tmnt4_battle.wav
-wav2msu -o tmnt4_msu1-57.pcm tmnt4_elevator.wav
-wav2msu -o tmnt4_msu1-42.pcm tmnt4_staff.wav
+wav2msu -o tmnt4_msu1-12.pcm -l 597637 tmnt4_pirate.wav
+wav2msu -o tmnt4_msu1-15.pcm -l 1466856 tmnt4_train.wav
+wav2msu -o tmnt4_msu1-18.pcm -l 674147 tmnt4_highway.wav
+wav2msu -o tmnt4_msu1-21.pcm -l 41322 tmnt4_starbase.wav
+wav2msu -o tmnt4_msu1-27.pcm -l 793700 tmnt4_lastboss.wav
+wav2msu -o tmnt4_msu1-235.pcm -l 153258 tmnt4_battle.wav
+wav2msu -o tmnt4_msu1-57.pcm -l 394790 tmnt4_elevator.wav
+wav2msu -o tmnt4_msu1-42.pcm -l 172310 tmnt4_staff.wav
 wav2msu -o tmnt4_msu1-253.pcm -l 681430 tmnt4_training.wav
 wav2msu -o tmnt4_msu1-69.pcm -l 546525 tmnt4_winner.wav
 wav2msu -o tmnt4_msu1-60.pcm -l 378744 tmnt4_memory.wav
